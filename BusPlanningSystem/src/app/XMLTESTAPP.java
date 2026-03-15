72f7df911ad52f7c9a600ff24e0d1a9742f957f3
Andrew Stevens <andrewstevens2015@gmail.com>
2026-03-15 17:02:09 -0400

    Test script and XML handling and manager established for just users

---
+++ BusPlanningSystem/src/app/XMLTESTAPP.java
@@ -0,0 +1,99 @@
+package app;
+
+import xml.UserManager;
+import java.util.Scanner;
+
+public class XMLTESTAPP {
+
+	public static void main(String[] args) throws Exception 
+	{
+		UserManager userManager = new UserManager();
+//		BusManager busManager = new BusManager();
+//		StationManager stationManager = new StationManager();
+		Scanner scanner = new Scanner(System.in);
+		int selection = -1;
+
+		System.out.println("XML TEST APP..................");
+		do
+		{
+			System.out.println("\n\n");
+			System.out.println("------------------------------------------------");
+			System.out.println("1. Print all users");
+			System.out.println("2. Print all buses");
+			System.out.println("3. Print all stations");
+			System.out.println("4. Add/remove users");
+			System.out.println("5. Add/remove buses");
+			System.out.println("6. Add/remove stations");
+			System.out.println("\n0. Exit\n\n");
+			System.out.print("Select option (0-6): ");
+			selection = Integer.parseInt(scanner.nextLine());
+			System.out.println("\n\n\n\n\n\n\n\n");
+			switch(selection)
+			{
+				case 1:
+					System.out.println("Printing users...");
+					userManager.printUserList();
+					break;
+				case 2:
+					System.out.println("Printing buses...");
+//					busManager.printBusList();
+					break;
+				case 3:
+					System.out.println("Printing stations...");
+//					stationManager.printStationList();
+					break;
+				case 4:
+					System.out.println("Do you want to add or remove a user?");
+					System.out.print("Select option (1 = ADD; 2 = REMOVE): ");
+					selection = Integer.parseInt(scanner.nextLine());
+					if(selection == 1)
+					{
+						System.out.println("\n\n\n\n\n\n\n\n");
+						System.out.print("Enter username: ");
+						String username = scanner.nextLine();
+						System.out.print("Enter password: ");
+						String password = scanner.nextLine();
+						System.out.print("Enter 1 if admin (0 by default): ");
+						boolean isAdmin = false;
+						if(Integer.parseInt(scanner.nextLine()) == 1)
+						{
+							isAdmin = true;
+						}
+						userManager.addUser(username, password, isAdmin);
+					}
+					else if(selection == 2)
+					{
+						System.out.println("\n\n\n\n\n\n\n\n");
+						System.out.print("Enter the ID of the user to remove: ");
+						int targetID = Integer.parseInt(scanner.nextLine());
+						boolean removedStatus = userManager.removeUser(targetID);
+						if(removedStatus == true)
+						{
+							System.out.println("User removed successfully!");
+						}
+						else
+						{
+							System.out.println("User not found!");
+						}
+					}
+					else
+					{
+						System.out.println("Invalid option, returning...");
+					}
+					break;
+				case 5:
+					//Add or remove buses here
+					break;
+				case 6:
+					//Add or remove stations here
+					break;
+				case 0:
+					System.out.println("Exiting program...");
+					break;
+				default:
+					System.out.println("Invalid option, returning...");
+			}
+		}while(selection != 0);
+	}
+
+}
